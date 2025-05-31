22282769f0f80bf25eb9137334865492a985ec20
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-05-31 21:11:36 +0100

    Oskar Davičo: Među Markosovim partizanima

---
+++ znaci-20250531.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202050531
+
+
+REPLACE INTO `knjige` VALUES 
+(654,1503,'Među Markosovim partizanima','Oskar Davičo','','','',140,1969,'',0,0,0,6,379);
+
+REPLACE INTO `sadrzaji` VALUES 
+(43300,'','','Cela knjiga (detalji u pripremi)',1,'1',1,1503,0);
