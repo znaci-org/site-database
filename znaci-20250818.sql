6a0566d6cc72245339d99324aec64b36e129aa76
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-08-17 20:26:22 +0100

    Teropšič nova knjiga

---
+++ znaci-20250818.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202050818
+
+REPLACE INTO `knjige` VALUES 
+(663,2236,'Prvi javni spomenik v Brežicah','Tomaž Teropšič','','','',257,2025,'',0,0,0,6,9);
+
+REPLACE INTO `sadrzaji` VALUES 
+(44200,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2236,0);
