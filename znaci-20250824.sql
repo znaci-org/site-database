7571102da8ffeedc69ba47b105487d73ead3681e
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-08-24 22:06:36 +0100

    Teropšič - spomenik četi

---
+++ znaci-20250824.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202050824
+
+REPLACE INTO `knjige` VALUES 
+(664,2377,'Spomenik Brežiški partizanski četi','Tomaž Teropšič','','','',257,2025,'',0,0,0,6,9);
+
+REPLACE INTO `sadrzaji` VALUES 
+(44200,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2377,0);
