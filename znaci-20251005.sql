025e8a64ece17e31bac19f17e8ad3b0aaf3ec0e0
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-10-05 14:03:39 +0100

    7 novih knjiga

---
+++ znaci-20251005.sql
@@ -0,0 +1,22 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202051005
+
+
+REPLACE INTO `knjige` VALUES 
+(669,2562,'Partizan Anton Medved','Tomaž Teropšič','','','',257,2025,'',0,0,0,3,661),
+(670,2563,'Partizan Maksimiljan Medved - Bojan','Tomaž Teropšič','','','',257,2025,'',0,0,0,3,365),
+(671,2564,'Ne vdaj se, fant','France Križnar','','','',202,1968,'',0,0,0,6,89),
+(672,2565,'Građanski rat u Srbiji 1941-1945','Borivoje M. Karapandžić','','','',257,2010,'',0,0,0,6,167),
+(673,2566,'Prilozi istoriji sukoba narodnooslobodilačkog pokreta i četnika Draže Mihailovića u Srbiji 1941','Jovan Marjanović','','','',59,1962,'',0,0,0,6,167),
+(674,2567,'Josip Broz Tito - Prilozi za biografiju','Vladimir Dedijer','','','',285,1955,'',0,0,0,3,167),
+(675,2568,'Uput za četničko ratovanje','Ministarstvo vojske i mornarice','','','',285,1929,'',0,0,0,6,167);
+
+REPLACE INTO `sadrzaji` VALUES 
+(44700,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2562,0),
+(44800,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2563,0),
+(44900,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2564,0),
+(45000,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2565,0),
+(45100,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2566,0),
+(45200,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2567,0),
+(45300,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2568,0);
