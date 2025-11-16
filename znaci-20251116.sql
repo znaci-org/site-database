74c63b7d43a8c6e27ae84d9e89352879794a7c96
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-11-16 10:22:47 +0000

    1 nova knjiga

---
+++ znaci-20251116.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202051116
+
+
+REPLACE INTO `knjige` VALUES 
+(676,2569,'Ilegalka Marija Teropšič','Tomaž Teropšič','','','',257,2025,'',0,0,0,3,661);
+
+REPLACE INTO `sadrzaji` VALUES 
+(45400,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2569,0);
