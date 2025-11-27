d396dbeca4308a45de0194f5d054313374cdc9e2
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2025-11-27 15:32:18 +0000

    Teropsic: info tabele

---
+++ znaci-20251127.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-202051127
+
+
+REPLACE INTO `knjige` VALUES 
+(677,2570,'Zgodovinske INFO table zveze borcev v Brežicah','Tomaž Teropšič','','','',257,2025,'',0,0,0,3,14);
+
+REPLACE INTO `sadrzaji` VALUES 
+(45400,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2570,0);
