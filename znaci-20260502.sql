1769e9889a085d4a44acb21bee6d9455634bdb3f
Znaci.org <106783989+znaci-org@users.noreply.github.com>
2026-05-02 21:30:15 +0100

    Novi Teropsic

---
+++ znaci-20260502.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted sql
+
+-- changeset liquibase:znaci-org-20260502
+
+REPLACE INTO `knjige` VALUES 
+(688,2581,'Uniforma domoljubne organizacije Zveze borcev Brežice','Tomaž Teropšič','','','',257,2026,'',0,0,0,6,10);
+
+REPLACE INTO `sadrzaji` VALUES 
+(46300,'','','Cela knjiga (detalji u pripremi)',1,'1',1,2581,0);
